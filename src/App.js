2285110468025783d112eb5b38abdebfc0169802
Punisher <abi.ft7@gmail.com>
2020-01-24 12:51:17 -0600

    Divided buttons into row arrays and rendered them in a table component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
 import DisplayButtons from "./DisplayButtons";
-//import "./style.css";
+import "./style.css";
 const x = 4;
 const App = () => {
   return (
-    <div className="calc-frame">
-      <div className="bottom">
-        <DisplayButtons />
+    <div className="app">
+      <div className="calc-frame">
+        <div className="bottom">
+          <DisplayButtons />
+        </div>
       </div>
     </div>
   );
--- src/DisplayButtons.js
+++ src/DisplayButtons.js
@@ -1,25 +1,47 @@
 import React from "react";
 import "./style.css";
 const DisplayButtons = props => {
-  const numberButtons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const operatorButtons = ["/", "*", "+", "-", "="];
+  const firstRow = [7, 8, 9, "/"];
+  const secondRow = [4, 5, 6, "*"];
+  const thirdRow = [1, 2, 3, "+"];
+  const fourthRow = [".", 0, "=", "-"];
+
+  const isLastItem = (item, row) => {
+    return item == row[row.length - 1] ? true : false;
+  };
 
   return (
     <>
-      <div>
-        {numberButtons.map(numberId => (
-          <button key={numberId} className="numberButton">
-            {numberId}
-          </button>
-        ))}
-      </div>
-      <div>
-        {operatorButtons.map(operatorId => (
-          <button key={operatorId} className="operatorButton">
-            {operatorId}
-          </button>
-        ))}
-      </div>
+      <table>
+        <tr>
+          {firstRow.map(numberId => (
+            <button key={numberId} className="grayButton">
+              {numberId}
+            </button>
+          ))}
+        </tr>
+        <tr>
+          {secondRow.map(numberId => (
+            <button key={numberId} className="grayButton">
+              {numberId}
+            </button>
+          ))}
+        </tr>
+        <tr>
+          {thirdRow.map(numberId => (
+            <button key={numberId} className="grayButton">
+              {numberId}
+            </button>
+          ))}
+        </tr>
+        <tr>
+          {fourthRow.map(numberId => (
+            <button key={numberId} className="grayButton">
+              {numberId}
+            </button>
+          ))}
+        </tr>
+      </table>
     </>
   );
 };
